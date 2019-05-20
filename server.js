fd9a0ba14301090f1bc9eb4b500c003df3c0e588
neillindberg <1066286+neillindberg@users.noreply.github.com>
2019-05-20 15:12:28 -0500

    Rate limit exceeded...
    Recursive to a limit retrieval of 5 followers (max) 3 levels deep (5/25/125 if the "first level" is the root or 625 if not??? follower listings from root max).

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const fetch = require('node-fetch');
+const httpCodes = require('http-codes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// https://api.github.com/users/<username>/following
+// https://api.github.com/users/<username>/followers
+
+const getFollowersUrl = username => `https://api.github.com/users/${username}/followers`;
+
+const fetchWithDepth = async (username, depth = 3) => {
+    const url = getFollowersUrl(username);
+    console.log('^^^^^^^^^^^URL^^^^^^^^^^^', url);
+    return await fetch(url)
+        .then(fetchResponse => fetchResponse.json())
+        .then(jsonResponse => {
+            return {
+                numFollowers: jsonResponse.length,
+                firstFiveFollowers: jsonResponse && jsonResponse.slice(0, 5).map(({ login }) => {
+                    return depth > 0 ? {[login]: fetchWithDepth(login, depth--)} : login;
+                }) || []
+            };
+        });
+};
+// Get first 5 followers, 3 levels deep...
+app.post('/get-5x3-report', async (req, res) => {
+    const initialUsername = req.body.username;
+    if (!initialUsername) res.end();
+    const report = await fetchWithDepth(initialUsername);
+    res.json(report);
+});
+
+server.listen(80);
